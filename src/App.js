0e1a01ddca66623cf52eb18f556e7d3f1492cffc
Torrin Hunter Bates <torrin4@gmail.com>
2020-03-16 12:25:47 -0400

    Fix refreshing page state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from "./Routes";
+import { Auth } from 'aws-amplify';
 
 function App() {
 
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {onload();}, []);
+
+  async function onload()
+  {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+
+    }
+    setIsAuthenticating(false);
+  }
 
   return (
     <Router>
