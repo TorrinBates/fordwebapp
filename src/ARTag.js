f7c34c1262afa14925e4422885342e64a31c69c9
Torrin Hunter Bates <torrin4@gmail.com>
2020-03-28 17:37:31 -0400

    Add drop downs

--- src/ARTag.js
+++ src/ARTag.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Switch from '@material-ui/core/Switch';
+import Select from 'react-select';
 
 const IOSSwitch = withStyles(theme => ({
     root: {
@@ -56,6 +57,17 @@ const IOSSwitch = withStyles(theme => ({
     );
 });
 
+const customStyles = {
+    control: (base, state) => ({
+      ...base,
+      boxShadow: state.isFocused ? "0 0 0 0.08rem #0055A5" : 0,
+      borderColor: state.isFocused ? '#0055A5' : base.borderColor,
+      '&:hover': {
+        borderColor: state.isFocused ? '#0055A5' : base.borderColor,
+      }
+    })
+};
+
 const useStyles = makeStyles(theme => ({
     Parent: {
         marginBottom: theme.spacing(1),
@@ -67,22 +79,36 @@ const useStyles = makeStyles(theme => ({
         fontSize: 18,
         marginRight: theme.spacing(2),
     },
+    Dropdown: {
+        width: theme.spacing(25),
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+      },
 }));
+
+const mediaOptions = [{ value: "Video", label: "Video" },{ value: "FAQ", label: "FAQ" }];
 
 export default function Tag(props) {
     const classes = useStyles();
     const [enabled, setEnabled] = useState(false);
+    const [primaryId, setPrimaryId] = useState(null);
+    const [secondaryId, setSecondaryId] = useState(null);
     const handleChange = event => {
         setEnabled(!enabled);
     };
+    let selectSecondary = (opt) => {
+        setSecondaryId(opt);
+    }
 
     return (
         <Box display="flex" alignItems="center" className={classes.Parent}>
             <img width="50" height="50" src={props.image} className={classes.Img} alt="AR Button Image."/>
             <b className={classes.Txt}>{props.feature}:</b>
-
             <Box flexGrow={1}/>
             <IOSSwitch checked={enabled} onChange={handleChange} name="checkedB" />
+            <Select options={mediaOptions} onChange={opt => selectSecondary(opt)} value={secondaryId} isClearable={true} styles={customStyles} className={classes.Dropdown}/>
+            <Select options={mediaOptions} onChange={opt => selectSecondary(opt)} value={secondaryId} isClearable={true} styles={customStyles} className={classes.Dropdown}/>
+            <Select options={mediaOptions} onChange={opt => selectSecondary(opt)} value={secondaryId} isClearable={true} styles={customStyles} className={classes.Dropdown}/>
         </Box>
     );
 }
